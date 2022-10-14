fb3105d7cb1578b072ef335cc5aff91623aec6b1
MujorB <mujorb@gmail.com>
2022-10-14 20:06:15 +0100

    Updates base model to include repr methods

--- models/base_model.py
+++ models/base_model.py
@@ -21,17 +21,38 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'update_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                                        kwargs['updated_at'],
+                                        '%Y-%m-%dT%H:%M:%S.%f'
+                                        )
+            else:
+                kwargs['updated_at'] = datetime.now()
+
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                                        kwargs['created_at'],
+                                        '%Y-%m-%dT%H:%M:%S.%f'
+                                        )
+            else:
+                kwargs['created_at'] = datetime.now()
+
+            if 'id' not in kwargs:
+                kwargs['id'] = str(uuid.uuid4())
+
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def __repr__(self):
+        """ Returns a formal string representation of instance """
+        return self.__str__()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
